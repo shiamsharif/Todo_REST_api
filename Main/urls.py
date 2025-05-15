2d251d923fc189715d74159cbbe61eb75b3c3e80
shiamsharif <shiam.sharif.07@gmail.com>
2025-05-15 20:40:52 +0600

    initial Commit

---
+++ Main/urls.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from django.urls import path, include
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView, SpectacularRedocView
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/', include('Todo.urls')),
+    
+    # New Add Schema:
+    path('schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    path('schema/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
+]
